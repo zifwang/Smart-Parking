39a13b6da7a6a236c3af9be0bbd5c41f88c2fd22
Zifan Wang <zifwang@Zifans-MacBook-Air.local>
2019-11-22 16:22:17 -0800

    add possible_plate

---
+++ src/license_plate_segmentation/possible_plates.py
@@ -0,0 +1,9 @@
+class possible_plates:
+    def __init__(self):
+        self.image_license_plate = None
+        self.image_gray = None
+        self.image_thresh = None
+
+        self.rr_loc_plate_in_scene = None
+
+        self.possible_chars = ''
